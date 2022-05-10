8e7b9199f8f9f5ca55f09a6161e6627aeedde1e1
bethany ann <bethanyann44@gmail.com>
2022-05-10 17:35:47 -0500

    initial project setup & db connection for test proj

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag'); //one of the dependencies of apollo server
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config');
+
+const server = new ApolloServer({ 
+    typeDefs,
+    resolvers
+})
+
+//connect to db before starting the server
+mongoose.connect(MONGODB, {useNewUrlParser: true}).then(() => {
+    console.log('MongoDB connected');
+
+    return server.listen({ port : 5000 });
+}).then(res => {
+    console.log(`server running at ${res.url}`);
+});
+
+
+// server.listen({ port: 5000 }).then(res => {
+//     console.log(`server running at ${res.url}`)
+// });
