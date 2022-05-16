1c287bf452e6a75c85548c6cbac7bde413ff35ee
bethany ann <bethanyann44@gmail.com>
2022-05-15 19:43:42 -0500

    front end wired up with apollo provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+import App from './App';
+
+const client = new ApolloClient({
+  // uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://localhost:5000',
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
